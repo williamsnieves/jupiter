31f138a9cac57751ad0b3301efca6296d78c138b
Willians Nieves <williamsnieves@gmail.com>
2019-06-20 16:31:58 +0200

    master - WIP - added module for orientation and gitignore

---
+++ src/modules/Orientation.js
@@ -0,0 +1,23 @@
+export default {
+    N: {
+      value: 'N',
+      left: 'O',
+      right: 'E'
+    },
+    O: {
+      value: 'O',
+      left: 'S',
+      right: 'N'
+    },
+    S: {
+      value: 'S',
+      left: 'E',
+      right: 'O'
+    },
+    E: {
+      value: 'E',
+      left: 'N',
+      right: 'S'
+    }
+  }
+  
